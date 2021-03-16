b3c0579af6f8d6d63246f52ead63cb4b4322f25e
Christine Godfrey <cldonnen2001@yahoo.com>
2021-03-16 06:27:15 -0500

    initial setup

---
+++ BasicClasses/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClasses
+{
+	// constructors
+	public class Car  // add access modifer "public", add class auto puts "class Car"
+	{
+		//constructors 1st and 2nd way
+		public Car()  // same name as the class = "Car" //default constuctor this is blank but needs properties fields
+		{
+
+		}
+
+		//3rd way passing thru constructor
+		public Car(string make, string model, int year)  // method overloading 2nd constructor,defined in class
+		{
+			Make = make;  // takes string make2 and stores in Make property
+			Model = model;
+			Year = year;
+		}
+		//fields  - example not here
+
+		//properites- example below
+		public string Make { get; set; }  //Create a Make property of type string that is public (ge = read, set = write)
+		public string Model { get; set; } //Create a Model property of type string that is public
+		public int Year { get; set; }//Create a Year property of type int that is public
+
+		//methods - example not here
+ 	}
+}
---
+++ BasicClasses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;  // add this when doing lists
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //dot. notation way
+            Car firstCar = new Car(); // Create a new instance of the Car class constructor{Car ()} method has new and (), type is Car = class name (not var)
+            firstCar.Make = "Honda";
+            firstCar.Model = "Element";
+            firstCar.Year = 2008;
+
+            // 2nd way object initializer way, new car
+            var mazda = new Car()  // new object called Mazda, usec Car class as blueprint
+            {
+                Make = "Mazda",
+                Model = "Who knows",
+                Year = 2052  // because I can
+            };
+
+            //3rd way passing thru constructor way
+            var jeep = new Car("Jeep", "Wrangler", 2174); //damn well better self drive
+
+            // print out
+            var carList = new List<Car>() { firstCar, mazda, jeep };  // List<type> remember type is name of class so = Car, List<car> constructor type Car
+
+            foreach (var carInfo in carList)
+            {
+                Console.WriteLine($"{carInfo.Make} {carInfo.Model} {carInfo.Year}");
+            }
+            
+        }
+
+
+    }
+}
